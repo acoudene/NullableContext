b983e3548b7b212593fb1a61d22837aef8c19ffc
Anthony Coudène <acoudene@users.noreply.github.com>
2023-01-06 18:45:09 +0100

    Update

--- NullableContext/EnableContextTest.cs
+++ NullableContext/EnableContextTest.cs
@@ -7,7 +7,7 @@ namespace NullableContext
 
 #nullable enable
     [Fact]
-    public void EnableContextTest_Warning_On_Nullable_Assignment()
+    public void EnableContextTest_On_Nullable_Assignment()
     {
       MyClass myClass = new MyClass();
 
@@ -21,7 +21,7 @@ namespace NullableContext
     }
 
     [Fact]
-    public void EnableContextTest_Warning_On_Nullable_Assignment_Forgiving_Operator()
+    public void EnableContextTest_On_Nullable_Assignment_And_Forgiving_Operator()
     {
       MyClass myClass = new MyClass();
 
@@ -33,7 +33,7 @@ namespace NullableContext
     }
 
     [Fact]
-    public void EnableContextTest_Warning_On_Null_Dereference_Forgiving_Operator()
+    public void EnableContextTest_On_Null_Dereference_And_Forgiving_Operator()
     {
       MyClass myClass = new MyClass();
 
@@ -45,11 +45,11 @@ namespace NullableContext
     }
 #nullable restore
 
-#nullable disable
+#nullable enable annotations
     [Fact]
-    public void DisableContextTest_Warning_On_Nullable_Dereference()
+    public void AnnotationsContextTest_On_Nullable_Assignment()
     {
-      MyClass myClass = new MyClass();
+      MyClass? myClass = new MyClass(); // No warning, ? is allowed
 
       // MyNullableData is declared as string?
       string myData = myClass.MyNullableData; // No warning
@@ -60,7 +60,7 @@ namespace NullableContext
     }
 
     [Fact]
-    public void DisableContextTest_Warning_On_Nullable_Dereference_Forgiving_Operator()
+    public void AnnotationsContextTest_On_Nullable_Assignment_And_Forgiving_Operator()
     {
       MyClass myClass = new MyClass();
 
@@ -70,13 +70,88 @@ namespace NullableContext
     }
 
     [Fact]
-    public void DisableContextTest_Warning_On_Null_Dereference_Forgiving_Operator()
+    public void AnnotationsContextTest_On_Null_Dereference_And_Forgiving_Operator()
     {
       MyClass myClass = new MyClass();
 
       // MyNullableData is declared as string?
       myClass.MyNullableData.GetHashCode(); // No warning
       myClass.MyNullableData!.GetHashCode(); // No warning, ! operator has no effect
+    }
+#nullable restore
+
+#nullable disable
+    [Fact]
+    public void DisableContextTest_On_Nullable_Assignment()
+    {
+      MyClass? myClass = new MyClass(); // Generate CS8632 warning on ? operator
+      // CS8632 not documented but the text: The annotation for nullable reference types should only be used in code within a '#nullable' context.
+
+      // MyNullableData is declared as string?
+      string myData = myClass.MyNullableData; // No warning
+
+      myClass.MyNullableData = "NotNull";
+
+      myData = myClass.MyNullableData; // No warning
+    }
+
+    [Fact]
+    public void DisableContextTest_On_Nullable_Assignment_And_Forgiving_Operator()
+    {
+      MyClass myClass = new MyClass();
+
+      // MyNullableData is declared as string?
+      string myData = myClass.MyNullableData; // No warning
+      myData = myClass.MyNullableData!; // No warning, ! operator has no effect
+    }
+
+    [Fact]
+    public void DisableContextTest_On_Null_Dereference_And_Forgiving_Operator()
+    {
+      MyClass myClass = new MyClass();
+
+      // MyNullableData is declared as string?
+      myClass.MyNullableData.GetHashCode(); // No warning
+      myClass.MyNullableData!.GetHashCode(); // No warning, ! operator has no effect
+    }
+#nullable restore
+
+
+#nullable enable warnings
+    [Fact]
+    public void WarningContextTest_On_Nullable_Assignment()
+    {
+      MyClass? myClass = new MyClass(); // Generate CS8632 warning on ? operator
+      // CS8632 not documented but the text: The annotation for nullable reference types should only be used in code within a '#nullable' context.
+
+      // MyNullableData is declared as string?
+      string myData = myClass.MyNullableData; // No warning on nullable Assignmment
+
+      myClass.MyNullableData = "NotNull";
+
+      myData = myClass.MyNullableData; // No warning
+    }
+
+    [Fact]
+    public void WarningContextTest_Warning_On_Nullable_Assignment_And_Forgiving_Operator()
+    {
+      MyClass myClass = new MyClass();
+
+      // MyNullableData is declared as string?
+      string myData = myClass.MyNullableData; // No warning on nullable Assignmment
+      myData = myClass.MyNullableData!; // No warning due to ! operator but you have to be sure about given nullable dereference
+    }
+
+    [Fact]
+    public void WarningContextTest_Warning_On_Null_Dereference_And_Forgiving_Operator()
+    {
+      MyClass myClass = new MyClass();
+
+      // MyNullableData is declared as string?
+      myClass.MyNullableData.GetHashCode(); /// Generate CS8602 warning 
+      /// <see cref="https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/nullable-warnings?f1url=%3FappId%3Droslyn%26k%3Dk(CS8602)#possible-dereference-of-null"/>
+      
+      myClass.MyNullableData!.GetHashCode(); // No warning due to ! operator but you have to be sure about given nullable dereference
     }
 #nullable restore
   }
--- NullableContext/MyClass.cs
+++ NullableContext/MyClass.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 
 namespace NullableContext
 {
+#nullable enable
   public class MyClass
   {    
     public string MyData { get; set; }
 
-#nullable enable
-    public string? MyNullableData { get; set; }
-#nullable restore
 
-    public MyClass()
+    public string? MyNullableData { get; set; }
+
+    public MyClass() // Generate a CS8618 warning due to not initialized MyData
+    /// <see cref="https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/nullable-warnings?f1url=%3FappId%3Droslyn%26k%3Dk(CS8618)#nonnullable-reference-not-initialized"/>
     {
-      MyData = "NotNull";
+      // To correct warning uncomment this
+      // MyData = "NotNull";
     }
   }
+#nullable restore
 }
